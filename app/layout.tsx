13c3bcfb8c1febbc845a7164ba332d3501bbc30f
Chris <pouradier.contact@gmail.com>
2025-01-31 15:11:59 +0100

    ajout de la font poppins comme font par defauts☂

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
+const poppins = Poppins({
+  weight: ['300', '400', '500', '600', '700'],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins"
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +29,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="fr" className = {poppins.className} >
+      <body>
         {children}
       </body>
     </html>
