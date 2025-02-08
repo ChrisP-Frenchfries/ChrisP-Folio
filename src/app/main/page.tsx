8b3e2f5a193d4bbb2c48cf4f8c1c8aa152767096
Chris <pouradier.contact@gmail.com>
2025-02-08 13:42:03 +0100

    ajout et intégration de composant TechcardsContainer qui fais office de section 2 de mon main page

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,5 +1,6 @@
 import { CardTitle } from "@/src/components/ui/card";
 import { MainSection } from "@/src/components/Main/MainSection";
+import { TechCardsContainer } from "@/src/components/MainSection2/TechCardsContainer";
 
 export default async function MainPage() {
     return (
@@ -13,7 +14,9 @@ export default async function MainPage() {
             <MainSection />
             {/* Vous pouvez ajouter d'autres sections ici */}
             <div className="border-t w-full max-w-4xl mx-auto "></div>
-
+            <CardTitle>SKILLS: /technologies-i-love-to-work-with </CardTitle>
+            <TechCardsContainer />
+            <div className="border-t w-full max-w-4xl mx-auto "></div>
         </main>
     )
 }
---
+++ src/components/MainSection2/TechCardsContainer.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import { TechCard } from "@/src/components/MainSection2/TechCard";
+
+export const TechCardsContainer: FC = () => {
+  return (
+    <div className="w-full max-w-4xl mx-auto flex flex-row items-center justify-between gap-4">
+      <TechCard
+        image="/images/tech1.png"
+        alt="Tech 1"
+        title="Tech 1"
+        description="Description pour Tech 1"
+      />
+      <TechCard
+        image="/images/tech2.png"
+        alt="Tech 2"
+        title="Tech 2"
+        description="Description pour Tech 2"
+      />
+      <TechCard
+        image="/images/tech3.png"
+        alt="Tech 3"
+        title="Tech 3"
+        description="Description pour Tech 3"
+      />
+    </div>
+  );
+};
