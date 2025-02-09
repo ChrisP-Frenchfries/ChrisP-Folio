2d5623af70e7b80019987ef0270d72e7beee9604
Chris <pouradier.contact@gmail.com>
2025-02-09 14:09:21 +0100

    restauration de la main page app

---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+import { CardTitle } from "@/src/components/ui/card";
+import { MainSection } from "@/src/components/Main/MainSection";
+import { TechCardsContainer } from "@/src/components/MainSection2/TechCardsContainer";
+import BlogPostsExample from "@/src/components/BlogPostsExemple";
+
+export default async function MainPage() {
+    return (
+        <main className="flex flex-col gap-6 p-4 ">
+
+            <div className="border-t w-full max-w-4xl mx-auto "></div>
+            <div className="w-full max-w-4xl  mx-auto " >
+                <CardTitle>URL: /Home </CardTitle>
+            </div>
+
+            <MainSection />
+            {/* Vous pouvez ajouter d'autres sections ici */}
+            <div className="border-t w-full max-w-4xl mx-auto "></div>
+            <div className="w-full max-w-4xl  mx-auto pb-4 pt-4" >
+                <CardTitle>SKILLS: /i-love-to-work-with:  </CardTitle>
+            </div>
+            <TechCardsContainer />
+            <div className="border-t w-full max-w-4xl mx-auto "></div>
+            <div className="w-full max-w-4xl  mx-auto pb-4 pt-4" >
+                <CardTitle>BLOG: /list:  </CardTitle>
+            </div>
+            <BlogPostsExample />
+            <div className="border-t w-full max-w-4xl mx-auto "></div>
+        </main>
+    )
+}
+
+// src/app/main/page.tsx
+
+
