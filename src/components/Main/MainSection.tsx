5851595b258512b335691988a1ac956c821d59d6
Chris <pouradier.contact@gmail.com>
2025-02-18 23:09:06 +0100

    debut d'ajout de la feature l'effet card hover

--- src/components/Main/MainSection.tsx
+++ src/components/Main/MainSection.tsx
@@ -34,24 +34,28 @@ export function MainSection() {
             imageUrl: "/images/job1.jpg",
             title: "cometdesign",
             description: "super description.",
+            imgHover: "/images/job1-hover.jpg",
         },
         {
             href: "/jobs/2",
             imageUrl: "/images/job2.jpg",
             title: "Job 2",
             description: "Description courte pour le Job 2.",
+            imgHover: "/images/job2-hover.jpg",
         },
         {
             href: "/jobs/3",
             imageUrl: "/images/job3.jpg",
             title: "Job 3",
             description: "Description courte pour le Job 3.",
+            imgHover: "/images/job3-hover.jpg",
         },
         {
             href: "/jobs/4",
             imageUrl: "/images/job4.jpg",
             title: "Job 4",
             description: "Description courte pour le Job 4.",
+            imgHover: "/images/job4-hover.jpg",
         },
     ];
 
@@ -59,7 +63,7 @@ export function MainSection() {
         <div className="w-full max-w-4xl mx-auto ">
             <div className="flex gap-4">
                 {/* Section Projets (7/12 de largeur) */}
-                <Card className="w-7/12 overflow-hidden p-4">
+                <Card className="w-7/12 overflow-hidden p-4 card-y">
                     <h2 className="text-xl font-bold mb-4">Mes Projets</h2>
                     <div className="flex flex-col gap-3">
                         {projects.map((project, idx) => (
